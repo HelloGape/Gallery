95133b5ccbbd78adc728ea0bc8b9619141c3cb24
Kang Jae Kwon <rkdwornjs123@gmail.com>
2019-01-18 20:45:51 +0900

    Start

---
+++ GalleryApp/GalleryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Kang JK on 18/01/2019.
+//  Copyright © 2019 Kang JK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
